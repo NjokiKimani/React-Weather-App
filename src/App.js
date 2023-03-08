ebc004e13ed4abc4371a9133fae123128a2ea650
NjokiKimani <maninjoki@gmail.com>
2023-03-08 14:32:49 -0800

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-  Weather
-      </header>
+      <div className="container">
+        <h1>Weather</h1>
+        <button className="btn btn-primary"> Start here</button>
+      </div>
     </div>
   );
 }
